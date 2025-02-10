e1356aae0dadce4a155dd23dc629293b588f2cc6
Advait Koparkar <advaitkoparkar@gmail.com>
2025-02-09 18:33:04 -0800

    cleanup

--- utils/schema/schema.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from abc import ABC, abstractmethod
-
-class Schema(ABC):
-    def __init__(self, ):
-        pass
-
-    def create_schema(self)
--- utils/schema/video_schema.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('../..')
-
-import sqlite3
-
-from
-
-class VideoSchema()
